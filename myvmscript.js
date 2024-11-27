31ca5ed140c0f43b64af38d3f6799972b902ecfe
Handy Hacker <handy@hackerhomestead.com>
2024-11-27 03:40:54 +0000

    Got Save State working, removed filessave but now know how to get it working based on save state

--- myvmscript.js
+++ myvmscript.js
@@ -22,16 +22,17 @@ window.onload = function()
 	// Define state, this is the object where we hold state
 	//  Idea: Could I have these in an array, so that I can have mulitable online-save states?
     	var state;
-
-	document.getElementById("save_restore").onclick = async function()
+	document.getElementById("saveMemory").onclick = async function()
 	    {
 		var button = this;
 
 		if(state)
 		{
 		    button.value = "Save state";
+		    button.textContent = "Save state";
 		    await emulator.restore_state(state);
-		    document.getElementById("log").value += "Restored from state\n";
+		    //document.getElementById("log").value += "Restored from state\n";
+		    console.log("Restored from state\n");
 		    state = undefined;
 		}
 		else
@@ -39,15 +40,15 @@ window.onload = function()
 		    const new_state = await emulator.save_state();
 		    
 		    console.log("Saved state of " + new_state.byteLength + " bytes");
-        	    document.getElementById("log").value += "Saved state of " + new_state.byteLength + " bytes\n";
+        	    //document.getElementById("log").value += "Saved state of " + new_state.byteLength + " bytes\n";
 		    
 	   	    button.value = "Restore state";
+		    button.textContent = "Restore State";
 		    state = new_state;
 		}
 
 		button.blur();
 	    };
-
 //////////////////////////////////////////////////
 //////////////////////////////////////////////////
 	document.getElementById("go_fullscreen").onclick = async function(){
@@ -55,25 +56,10 @@ window.onload = function()
 			//emulator.screen_set_scale(1,1);
 	}
 
-	// We are going to see if we can add the reset CPU call/button
-	document.getElementById("ResetCPU").onclick = async function(){
-			emulator.restart();
-	}
 
 //////////////////////////////////////////////////
 //////////////////////////////////////////////////
-	document.getElementById("save_file").onclick = async function(){
-		const new_state = await emulator.save_state();
-		var a = document.createElement("a");
-		a.download = "v86state.bin";
-		a.href = window.URL.createObjectURL(new Blob([new_state]));
-		a.dataset.downloadurl = "application/octet-stream:" + a.download + ":" + a.href;
-		a.click();
-
-		this.blur();
-	};
-
-	    document.getElementById("restore_file").onchange = function()
+/**	    document.getElementById("restore_file").onchange = function()
 	    {
 		if(this.files.length)
 		{
@@ -93,7 +79,7 @@ window.onload = function()
 
 		this.blur();
 	    };
-
+**/
 //////////////////////////////////////////////////
 //////////////////////////////////////////////////
     // In this example we wait for output from the serial terminal,
@@ -119,7 +105,38 @@ window.onload = function()
 //////////////////////////////////////////////////
 };
 
-function rCPU(){
+function resetCPU(){
   	emulator.restart();
 	console.log("Reset CPU Requested\n");
 }
+
+function saveMemory(){
+	console.log("Request to Save Memory");
+}
+
+/**
+function stateFileSave(){
+	const new_state = emulator.save_state();
+	var a = document.createElement("a");
+	a.download = "v86state.bin";
+	a.href = window.URL.createObjectURL(new Blob([new_state]));
+	a.dataset.downloadurl = "application/octet-stream:" + a.download + ":" + a.href;
+	a.click();
+}
+
+function stateFileRestore(){
+	var b = document.createElement("b");
+	//if(b.files.length){
+	    var filereader = new FileReader();
+	    emulator.stop();
+	    filereader.onload = function(e){
+		emulator.restore_state(e.target.result);
+		emulator.run();
+	    };
+	    filereader.readAsArrayBuffer(b.files[0]);
+	    b.value = "";
+	//}
+	
+}
+**/
+
